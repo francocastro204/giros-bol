45f4bdf98ff4d59508dfb7d769de9534a465ab1a
Franco Castro Miranda <dev.francoscm@gmail.com>
2025-01-05 21:24:08 -0300

    feat: se agrega archivo de robots y sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://girosbol.com', // Reemplaza con tu dominio
+    generateRobotsTxt: true, // Genera también un robots.txt
+    changefreq: 'daily', // Frecuencia de cambio
+    priority: 0.7, // Prioridad de las páginas
+    sitemapSize: 5000, // Máximo de URLs por sitemap
+}
